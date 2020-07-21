9e469b272c760200180ee241e8e231b8cf61c1c9
jmamadeu <jmamadeu2000@gmail.com>
2020-07-21 23:54:03 +0100

    :zap: chore: Finish the version 1 of populate DB feature

--- backend/src/populateDb.ts
+++ backend/src/populateDb.ts
@@ -1,8 +1,10 @@
-import { getConnection, getRepository } from 'typeorm';
+import { getRepository } from 'typeorm';
 
 import dbConnection from './database';
 
 import Province from './database/entities/Province';
+import Municipality from './database/entities/Municipality';
+import CommuneDistrict from './database/entities/CommuneDistrict';
 
 import dataJSON from './database/data/data.json';
 
@@ -10,17 +12,52 @@ import dataJSON from './database/data/data.json';
   await dbConnection();
 
   const provinceRepository = getRepository(Province);
+  const municipalityRepository = getRepository(Municipality);
+  const communesDistrictsRepository = getRepository(CommuneDistrict);
 
   await provinceRepository.createQueryBuilder().delete().execute();
+  await municipalityRepository.createQueryBuilder().delete().execute();
+  await communesDistrictsRepository.createQueryBuilder().delete().execute();
 
-  await Promise.all(
-    dataJSON.provinces.map(async (province) => {
-      const provinceToSave = provinceRepository.create({
-        name: province.name,
-        is_capital: province.name === 'Luanda' ? true : false,
-      });
+  try {
+    await Promise.all(
+      dataJSON.provinces.map(async (province) => {
+        const provinceToSave = provinceRepository.create({
+          name: province.name,
+          code: province.code,
+          is_capital: province.name === 'Luanda' ? true : false,
+          iso_code: province.iso_code,
+        });
 
-      await provinceRepository.save(provinceToSave);
-    })
-  );
+        await provinceRepository.save(provinceToSave);
+
+        await Promise.all(
+          province.municipalities.map(async (municipality) => {
+            const municipalityToSave = municipalityRepository.create({
+              name: municipality.name,
+              province: provinceToSave,
+            });
+
+            await municipalityRepository.save(municipalityToSave);
+
+            municipality.communes_districts.map(
+              async (communeDistrict: CommuneDistrict) => {
+                const communeDistrictToSave = communesDistrictsRepository.create(
+                  {
+                    name: communeDistrict.name,
+                    municipality: municipalityToSave,
+                    type: communeDistrict.type,
+                  }
+                );
+
+                await communesDistrictsRepository.save(communeDistrictToSave);
+              }
+            );
+          })
+        );
+      })
+    );
+  } catch (err) {
+    console.log(err);
+  }
 })();
